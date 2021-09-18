04dd11a72f353235485f4bf683e86fa95f0a8a75
Mo <mfardin@students.cs.ubc.ca>
2021-09-18 15:46:36 -0700

    Converted methods in angelcalc.py to static methods

--- anglecalc.py
+++ anglecalc.py
@@ -1,28 +1,29 @@
-def refAngle(angle):
-    '''
-    Finds the reference angle [0, 359] for a given angle
-    :param angle: float
-    :return: float
-    '''
-    return (360 + angle % 360) % 360
-
-
 class AngleCalc:
+    @staticmethod
+    def refAngle(angle):
+        '''
+        Finds the reference angle [0, 369] for a given angle
+        :param angle: float
+        :return: float
+        '''
+        return (360 + angle % 360) % 360
 
-    def boundTo180(self, angle):
+    @staticmethod
+    def boundTo180(angle):
         '''
         Bounds the provided angle in degrees between [-180, 179] degrees
         :param angle: float
         :return: float
         '''
-        angle = refAngle(angle)
+        angle = AngleCalc.refAngle(angle)
         if angle == 180:
             angle = -180
         elif angle > 180:
             angle = angle % -180
         return angle
 
-    def isAngleBetween(self, first_angle, middle_angle, second_angle):
+    @staticmethod
+    def isAngleBetween(first_angle, middle_angle, second_angle):
         '''
         Determines whether middle_angle is in between two bounding angles
         :param first_angle: float
@@ -30,8 +31,8 @@ class AngleCalc:
         :param second_angle: float
         :return: bool
         '''
-        first_angle = refAngle(first_angle)
-        middle_angle = refAngle(middle_angle)
-        second_angle = refAngle(second_angle)
+        first_angle = AngleCalc.refAngle(first_angle)
+        middle_angle = AngleCalc.refAngle(middle_angle)
+        second_angle = AngleCalc.refAngle(second_angle)
 
         return first_angle < middle_angle and middle_angle < second_angle
--- test_angelcalc.py
+++ test_angelcalc.py
@@ -1,56 +1,63 @@
 import unittest
-from anglecalc import *
+from anglecalc import AngleCalc
+
 
 class TestAngleCalc(unittest.TestCase):
 
     def test_refAngle(self):
-        result1 = refAngle(90)
+        result0 = AngleCalc.refAngle(0)
+        self.assertEqual(result0, 0)  # no change
+
+        result1 = AngleCalc.refAngle(90)
         self.assertEqual(result1, 90)  # no change
 
-        result2 = refAngle(360)
+        result2 = AngleCalc.refAngle(360)
         self.assertEqual(result2, 0)   # edge case
 
-        result3 = refAngle(630)
+        result3 = AngleCalc.refAngle(630)
         self.assertEqual(result3, 270) # >360 w/ ref angle >180
 
-        result4 = refAngle(765)
+        result4 = AngleCalc.refAngle(765)
         self.assertEqual(result4, 45)  # >360 w/ ref angle <180
 
     def test_boundTo180(self):
-        result1 = AngleCalc.boundTo180(self, 0)
+        result1 = AngleCalc.boundTo180(0)
         self.assertEqual(result1, 0)    # no change, positive
 
-        result2 = AngleCalc.boundTo180(self, 179)
+        result2 = AngleCalc.boundTo180(179)
         self.assertEqual(result2, 179)  # no change, edge case
 
-        result3 = AngleCalc.boundTo180(self, 180)
+        result3 = AngleCalc.boundTo180(180)
         self.assertEqual(result3, -180) # exception (when angle is 180)
 
-        result4 = AngleCalc.boundTo180(self, -180)
+        result4 = AngleCalc.boundTo180(-180)
         self.assertEqual(result4, -180) # exception (when angle is -180)
 
-        result5 = AngleCalc.boundTo180(self, 225)
+        result5 = AngleCalc.boundTo180(225)
         self.assertEqual(result5, -135) # positive to negative; reference angle >180
 
-        result6 = AngleCalc.boundTo180(self, -90)
+        result6 = AngleCalc.boundTo180(-90)
         self.assertEqual(result6, -90)  # no change, negative
 
-        result7 = AngleCalc.boundTo180(self, -315)
+        result7 = AngleCalc.boundTo180(-315)
         self.assertEqual(result7, 45)   # negative to positive; reference angle <180
 
-        result8 = AngleCalc.boundTo180(self, 765)
+        result8 = AngleCalc.boundTo180(765)
         self.assertEqual(result8, 45)   # find reference angle; no change in sign
 
-        result9 = AngleCalc.boundTo180(self, 630)
+        result9 = AngleCalc.boundTo180(630)
         self.assertEqual(result9, -90)  # find reference angle; change in sign
 
     def test_isAngleBetween(self):
-        self.assertTrue(AngleCalc.isAngleBetween(self, 0, 45, 90))     # True; all angles <180
+        self.assertTrue(AngleCalc.isAngleBetween(0, 45, 90))     # True; all angles <180
 
-        self.assertTrue(AngleCalc.isAngleBetween(self, 90, 179, -135)) # True; edge case and negative angle
+        self.assertTrue(AngleCalc.isAngleBetween(90, 179, -135)) # True; edge case and negative angle
 
-        self.assertTrue(AngleCalc.isAngleBetween(self, 135, 180, 270)) # True; exception and angle >180
+        self.assertTrue(AngleCalc.isAngleBetween(135, 180, 270)) # True; exception and angle >180
 
-        self.assertFalse(AngleCalc.isAngleBetween(self, 0, 45, 20))    # False; no angle conversion
+        self.assertFalse(AngleCalc.isAngleBetween(0, 45, 20))    # False; no angle conversion
 
-        self.assertFalse(AngleCalc.isAngleBetween(self, 180, 270, 90)) # False; w/ angle conversion
+        self.assertFalse(AngleCalc.isAngleBetween(180, 270, 90)) # False; w/ angle conversion
+
+if __name__ == '__main__':
+    unittest.main()
